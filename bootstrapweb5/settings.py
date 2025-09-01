4d0fbecec51f75ca604f24b8296c1d36decd6162
bXiilzy <jiranankredkla12@gmail.com>
2025-09-01 09:43:55 +0700

    data

--- bootstrapweb5/settings.py
+++ bootstrapweb5/settings.py
@@ -74,15 +74,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'bootstrapweb5.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#          'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#  }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('SUPABASE_DB_PASSWORD', 'b010105JK'),
+        'HOST': 'zixmsgrgqrkjdfqkgmsm.supabase.co',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
